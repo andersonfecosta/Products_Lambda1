6810fe4f5637e330c5082d6bf1ab9790e614f112
Anderson Costa <andersonfelipedc@gmail.com>
2023-01-18 09:44:44 -0300

    Final commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,8 +6,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
@@ -23,25 +25,28 @@ public class Program {
             String line = br.readLine();
             while (line != null) {
                 String[] fields = line.split(",");
-                products.add(new Product(fields[0],Double.parseDouble(fields[1])));
+                products.add(new Product(fields[0], Double.parseDouble(fields[1])));
                 line = br.readLine();
             }
-            double sum = 0.0;
-            for (Product p: products) {
-                sum += p.getPrice();
-            }
-            double average = sum/ products.size();
-            System.out.printf("Average price: %.2f",average);
+
+            double average = products.stream().map(p -> p.getPrice())
+                    .reduce(0.0, (x,y) -> x+y) / products.size();
+
+            Comparator<String> comp = (s1,s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+            System.out.printf("Average price: %.2f", average);
             System.out.println();
-            for (Product p: products) {
-                if (p.getPrice() < average) {
-                    System.out.println(p.getName());
-                }
+
+            List<String> names = products.stream()
+                    .filter(p -> p.getPrice() < average)
+                    .map(p -> p.getName())
+                    .sorted(comp.reversed())
+                    .collect(Collectors.toList());
+
+            names.forEach(System.out::println);
+
+            } catch(IOException e){
+                System.out.println("Error: " + e.getMessage());
             }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());;
         }
-
-
     }
-}
